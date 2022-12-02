ff62a9fc915c38d03840cb5250359ddcf3cee923
bharatchaudhary18998 <44448637+bharatchaudhary18998@users.noreply.github.com>
2022-12-02 14:11:49 +0530

    Update CicdPipeLineApplication.java

--- src/main/java/com/CicdPipeLineApplication.java
+++ src/main/java/com/CicdPipeLineApplication.java
@@ -1,1 +1,13 @@
+package com;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdPipeLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdPipeLineApplication.class, args);
+	}
+
+}
